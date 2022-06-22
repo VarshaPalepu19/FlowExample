8bd852950aa3236f333c2bae990b144129274792
VarshaPalepu19 <69585002+VarshaPalepu19@users.noreply.github.com>
2022-06-22 12:50:20 +0530

    Add files via upload

---
+++ flowScreen.js
@@ -0,0 +1,5 @@
+import { LightningElement,api} from 'lwc';
+//import getDetails from '@salesforce/apex/recordForm.getDetails';
+export default class FlowScreen extends LightningElement {
+   @api recordId;
+}
